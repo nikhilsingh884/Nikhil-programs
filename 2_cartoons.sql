5a6026ea4394e4c7db9cd73252ddbdeb66ec21fb
Nikhil Singh <110892859+nikhilsingh884@users.noreply.github.com>
2022-09-30 15:58:40 +0530

    Add files via upload

---
+++ 2_cartoons.sql
@@ -0,0 +1,13 @@
+use nikhil;
+
+create table Cartoons(
+First_Name varchar(20),
+Last_Name varchar(20),
+Address varchar(50),
+City varchar(50),
+Age int);
+
+insert into cartoons(
+First_name,last_name,address, city, age)Values('tweety','bird','543 xyz street','disney', 44);
+
+select * from cartoons;
